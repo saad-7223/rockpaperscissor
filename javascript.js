231e3509a7e783f34729a8bbfa6b2e1d2b2cbb0a
saad-7223 <saadsyed837@gmail.com>
2024-02-08 18:33:14 +0530

    Added eventListners to the buttons

--- javascript.js
+++ javascript.js
@@ -1,70 +1,82 @@
-console.log("hello world from javascript file ")
+let r_btn = document.querySelector(".rock")
+let s_btn = document.querySelector(".scissors")
+let p_btn = document.querySelector(".paper")
 
-function getComputerChoice(){
-    p = Math.floor(Math.random()*3)
-    if (p === 0){
-        return 'ROCK'
-    }
-    if (p === 1){
-        return 'PAPER'
-    }
-    if (p === 2){
-        return 'SCISSOR'
-    }
-}
 
-function playRound(player,computer){
+r_btn.addEventListener('click',()=>{
+    console.log(r_btn.textContent)
+})
+s_btn.addEventListener('click',()=>{
+    console.log(s_btn.textContent)
+})
+p_btn.addEventListener('click',()=>{
+    console.log(p_btn.textContent)
+})
+// function getComputerChoice(){
+//     p = Math.floor(Math.random()*3)
+//     if (p === 0){
+//         return 'ROCK'
+//     }
+//     if (p === 1){
+//         return 'PAPER'
+//     }
+//     if (p === 2){
+//         return 'SCISSOR'
+//     }
+// }
 
-    let p = player.toUpperCase()
-    let c = computer
+// function playRound(player,computer){
 
-    // INCASE OF A TIE 
-    if (p === c){
-        c = getComputerChoice()
-    }
-    if(p === 'ROCK' && c === 'PAPER'){
-        return 1
-    }
-    if(p === 'PAPER' && c === 'SCISSOR'){
-        return 1
-    }
-    if(p === 'SCISSOR' && c === 'ROCK'){
-        return 1
-    }
-    if(p === 'ROCK' && c === 'SCISSOR'){
-        return -1
-    }
-    if(p === 'PAPER' && c === 'ROCK'){
-        return -1
-    }
-    if(p === 'SCISSOR' && c === 'PAPER'){
-        return -1
-    }
-}
+//     let p = player.toUpperCase()
+//     let c = computer
 
-function game(n){
+//     // INCASE OF A TIE 
+//     if (p === c){
+//         c = getComputerChoice()
+//     }
+//     if(p === 'ROCK' && c === 'PAPER'){
+//         return 1
+//     }
+//     if(p === 'PAPER' && c === 'SCISSOR'){
+//         return 1
+//     }
+//     if(p === 'SCISSOR' && c === 'ROCK'){
+//         return 1
+//     }
+//     if(p === 'ROCK' && c === 'SCISSOR'){
+//         return -1
+//     }
+//     if(p === 'PAPER' && c === 'ROCK'){
+//         return -1
+//     }
+//     if(p === 'SCISSOR' && c === 'PAPER'){
+//         return -1
+//     }
+// }
 
-    let player_score = 0
-    let computer_score = 0
+// function game(n){
+
+//     let player_score = 0
+//     let computer_score = 0
     
-    for (i=0;i<n;i++){
-        let player = prompt("Enter Rock/Paper/SCISSOR :")
-        let r = playRound(player,getComputerChoice())
-        if (r===-1){
-            player_score++
-        }
-        else{
-            computer_score++
-        }
-    }
+//     for (i=0;i<n;i++){
+//         let player = prompt("Enter Rock/Paper/SCISSOR :")
+//         let r = playRound(player,getComputerChoice())
+//         if (r===-1){
+//             player_score++
+//         }
+//         else{
+//             computer_score++
+//         }
+//     }
 
-    if (player_score > computer_score){
-        console.log("player won by "+player_score+" points")
-    }else if (player_score < computer_score){
-        console.log("computer won by "+computer_score+" points")
-    }else{
-        console.log("It's a Tie")
-    }
-}
+//     if (player_score > computer_score){
+//         console.log("player won by "+player_score+" points")
+//     }else if (player_score < computer_score){
+//         console.log("computer won by "+computer_score+" points")
+//     }else{
+//         console.log("It's a Tie")
+//     }
+// }
 
-game(5)
+// game(5)
